285f9633e9a7ad15778e5ebb56d8afa786233573
MUNEZERO-Cindy <cindyyymun55@gmail.com>
2026-03-01 18:50:19 +0200

    feat(dto): add LocationDTO for data transfer

--- src/main/java/auca/ac/rw/carRentalHub/dto/LocationDTO.java
+++ src/main/java/auca/ac/rw/carRentalHub/dto/LocationDTO.java
@@ -1,5 +1,46 @@
 package auca.ac.rw.carRentalHub.dto;
 
-public class LocationDTO {
+import java.util.UUID;
+import java.util.List;
 
+public class LocationDTO {
+    private UUID id;
+    private String code;
+    private String name;
+    private String type;
+    private String parentName;
+    private String parentId;
+    private List<LocationDTO> children;
+
+    // Constructors
+    public LocationDTO() {}
+
+    public LocationDTO(UUID id, String code, String name, String type) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getParentName() { return parentName; }
+    public void setParentName(String parentName) { this.parentName = parentName; }
+
+    public String getParentId() { return parentId; }
+    public void setParentId(String parentId) { this.parentId = parentId; }
+
+    public List<LocationDTO> getChildren() { return children; }
+    public void setChildren(List<LocationDTO> children) { this.children = children; }
 }
