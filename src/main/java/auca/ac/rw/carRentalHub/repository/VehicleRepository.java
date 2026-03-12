4026e090b96c23c6e0da29326db766affd2d3283
MUNEZERO-Cindy <cindyyymun55@gmail.com>
2026-03-12 13:30:51 +0200

    Fixed JPA query in VehicleRepository - changed vf.feature.id to f.id

--- src/main/java/auca/ac/rw/carRentalHub/repository/VehicleRepository.java
+++ src/main/java/auca/ac/rw/carRentalHub/repository/VehicleRepository.java
@@ -9,7 +9,7 @@ import auca.ac.rw.carRentalHub.model.Vehicle;
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
     
     // find vehicles that have a given feature (by feature id)
-    @org.springframework.data.jpa.repository.Query("SELECT v FROM Vehicle v JOIN v.features vf WHERE vf.feature.id = :featureId")
+    @org.springframework.data.jpa.repository.Query("SELECT v FROM Vehicle v JOIN v.features f WHERE f.id = :featureId")
     org.springframework.data.domain.Page<Vehicle> findByFeatureId(@org.springframework.data.repository.query.Param("featureId") UUID featureId, org.springframework.data.domain.Pageable pageable);
 
 }
